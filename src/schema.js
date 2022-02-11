e2d168d8b70182c49a6c1261a0e82a8df560fe68
慢吞吞 <wenweixin.maxman@foxmail.com>
2022-02-12 02:24:13 +0800

    add deprecate

--- src/schema.js
+++ src/schema.js
@@ -64,7 +64,7 @@ const typeDefs = gql`
     "The module's title"
     title: String!
     "The module's length in minutes"
-    length: Int
+    length: Int @deprecated(reason: "Use durationInSeconds")
     "The module's text-based description, can be in markdown format. In case of a video, it will be the enriched transcript"
     content: String
     "The module's video url, for video-based modules"
